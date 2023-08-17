4b1e8d1ed07a069b170f24f10b5a5b0dee83f582
Fascetta <christianbianchi23@gmail.com>
2023-08-17 21:15:46 +0200

    Changed some values due to tuning

--- code/refine.py
+++ code/refine.py
@@ -14,7 +14,7 @@ def detect_edges(image_path):
 
         # Define the lower and upper bounds of the yellow color in HSV
         lower_yellow = np.array([20, 100, 100], dtype=np.uint8)
-        upper_yellow = np.array([25, 255, 255], dtype=np.uint8)
+        upper_yellow = np.array([30, 255, 255], dtype=np.uint8)
 
         # Create a binary mask where pixels falling within the yellow color range become white (255) and others black (0)
         mask = cv.inRange(hsv_image, lower_yellow, upper_yellow)
@@ -30,7 +30,7 @@ def detect_edges(image_path):
             # cv.imwrite("cropped_card.jpg", card[y:y+h, x:x+w])
 
             # Return the cropped region of the cropped mask and the bounding rectangle coordinates
-            return mask[y:y+h, x:x+w], (x, y, w, h), card[y:y+h, x:x+w][h//6 : h//2, w//8 : (8*w)//10], card[y:y+h, x:x+w][h//2 : (5*h)//8 , (5*w)//6 : (16*w)//17]
+            return mask[y:y+h, x:x+w], (x, y, w, h), card[y:y+h, x:x+w][h//6 : h//2, w//8 : (8*w)//10], card[y:y+h, x:x+w][h//2 : (h*3)//5 , (w*5)//6 : (w*19)//20]
         else:
             # Return None if no yellow object is detected
             return None, None, None, None
