efaed8c8c95bf2cdbb18fc2515a578ee4218110f
MastroCC8 <nicolamastrorilli33@gmail.com>
2023-08-30 17:57:18 +0200

    Update GUI.py
    adaptable frame

--- code/GUI.py
+++ code/GUI.py
@@ -6,12 +6,12 @@ def start():
     a_notebook.tab(card_depiction_frame, state="normal")
     a_notebook.select(card_depiction_frame)
 
-''' Nicola's function that ChatGPT cancelled but it doesn't run yet so we don't know if it works
+#Nicola's function that ChatGPT cancelled but it doesn't run yet so we don't know if it works
 def kyc():
     if input_form.get().endswith('.jpg'):
         kyc_button.config(state="disabled")
         new_pokemon=Image.open(input_form.get())
-        resized=new_pokemon.resize((300,375),Image.ANTIALIAS)
+        resized=new_pokemon.resize((300,375))
         card_pic=ImageTk.PhotoImage(resized)
         image_label=tk.Label(card_frame,image=card_pic)
         image_label.image=card_pic #Very important to display the photo correctly
@@ -30,14 +30,14 @@ def kyc():
         instructions.place_forget()
         input_form.place_forget()
         kyc_button.place_forget()
-        here_it_is=tk.PhotoImage(file="images/GUI/Logo_placeholder.png") #da sostituire con scritta here's your card! con font bellini che riprendono il logo 
+        here_it_is=tk.PhotoImage(file="images/GUI/Logo.png") #da sostituire con scritta here's your card! con font bellini che riprendono il logo 
         Heres_your_card=tk.Label(upper_frame,image=here_it_is)
         Heres_your_card.image=here_it_is
         Heres_your_card.place(relx=0.5,rely=0.5,anchor="center")
     else:
         pass
-'''
 
+'''
 def kyc():
     image_path = input_form.get()
     if image_path.endswith('.jpg'):
@@ -61,7 +61,7 @@ def kyc():
         
     else:
         pass
-
+'''
 def home():
     a_notebook.select(starting_frame)
     a_notebook.tab(card_depiction_frame, state="hidden")
@@ -69,7 +69,7 @@ def home():
 # Initialize the main application
 root = tk.Tk()
 root.title("KYC")
-root.geometry("800x800")
+root.geometry("800x600")
 root.iconbitmap("images\GUI\Logo_wind.ico")
 
 # Notebook initialization
@@ -88,8 +88,8 @@ a_notebook.add(card_depiction_frame, text="Card")
 a_notebook.tab(card_depiction_frame, state="hidden")
 
 # Structure of the input form
-upper_frame = tk.LabelFrame(card_depiction_frame, height=115, width=800, border=0)
-upper_frame.place(relx=0, rely=0)
+upper_frame = tk.LabelFrame(card_depiction_frame, border=0,background="red")
+upper_frame.place(relx=0, rely=0,relwidth=1, relheight=0.2)
 instructions = tk.Label(upper_frame, text="Insert the path to the image of the card you want to analyze and then click the button below")
 instructions.place(relx=0.5, rely=0.2, anchor="center")
 input_form = tk.Entry(upper_frame, width=100)
@@ -100,38 +100,38 @@ home_button = tk.Button(card_depiction_frame, text='HOME', command=home)
 home_button.place(relx=0.003, rely=0.997, anchor="sw")
 
 # Structure of the frames in the card depiction page
-card_frame = tk.LabelFrame(card_depiction_frame, height=400, width=300, border=0)
-card_frame.place(relx=0.003, rely=0.95, anchor="sw")
+card_frame = tk.LabelFrame(card_depiction_frame, border=0,background='orange')
+card_frame.place(relx=0, rely=0.3, relwidth=0.35,relheight=0.65, anchor="nw")
 vote_frame = tk.LabelFrame(card_depiction_frame, height=50, width=300, border=2)
-vote_frame.place(relx=0.003, rely=0.2, anchor="nw")
-description_frame = tk.LabelFrame(card_depiction_frame, height=430, width=495, border=1)
-description_frame.place(relx=0.378, rely=0.2, anchor="nw")
+vote_frame.place(relx=0, rely=0.2, relwidth=0.35,relheight=0.1, anchor="nw")
+name_frame=tk.LabelFrame(card_depiction_frame,background='yellow')
+name_frame.place(relx=0.35,rely=0.2, relheight=0.1,relwidth=0.65,anchor='nw')
+description_frame = tk.LabelFrame(card_depiction_frame, border=1,background='purple')
+description_frame.place(relx=0.35, rely=0.3, relheight=0.65,relwidth=0.65, anchor="nw")
 
 # Frames in the vote frame
 # Frames for each individual vote
-centering_frame = tk.LabelFrame(vote_frame, width=125, height=25, border=0)
-centering_frame.place(relx=0, rely=0, anchor="nw")
-edges_frame = tk.LabelFrame(vote_frame, width=125, height=25, border=0)
-edges_frame.place(relx=0, rely=1, anchor="sw")
-corners_frame = tk.LabelFrame(vote_frame, width=125, height=25, border=0)
-corners_frame.place(relx=0.4166, rely=0, anchor="nw")
-surface_frame = tk.LabelFrame(vote_frame, width=125, height=25, border=0)
-surface_frame.place(relx=0.4166, rely=1, anchor="sw")
+centering_frame = tk.LabelFrame(vote_frame,  border=0)
+centering_frame.place(relx=0, rely=0,relheight=0.5,relwidth=0.4, anchor="nw")
+edges_frame = tk.LabelFrame(vote_frame,  border=0)
+edges_frame.place(relx=0, rely=0.5, relheight=0.5,relwidth=0.4, anchor="nw")
+corners_frame = tk.LabelFrame(vote_frame,  border=0)
+corners_frame.place(relx=0.4, rely=0, relheight=0.5,relwidth=0.4, anchor="nw")
+surface_frame = tk.LabelFrame(vote_frame,   border=0)
+surface_frame.place(relx=0.4, rely=0.5, relheight=0.5,relwidth=0.4, anchor="nw")
 # Frame for the mean aka final vote
-mean_frame = tk.LabelFrame(vote_frame, width=50, height=45, border=0)
-mean_frame.place(relx=0.8332, rely=0, anchor="nw")
+mean_frame = tk.LabelFrame(vote_frame,  border=0 )
+mean_frame.place(relx=0.8, rely=0, relheight=1,relwidth=0.2, anchor="nw")
 
 # Structure of the description frame
-name_frame = tk.LabelFrame(description_frame, height=50, width=495)
-name_frame.pack()
-ability_frame = tk.LabelFrame(description_frame, height=129, width=300)
-ability_frame.pack()
-moves_frame = tk.LabelFrame(description_frame, height=129, width=300)
-moves_frame.pack()
-weakness_frame = tk.LabelFrame(description_frame, height=129, width=495)
-weakness_frame.pack()
-another_frame = tk.LabelFrame(description_frame, height=258, width=195)
-another_frame.pack()
+ability_frame = tk.LabelFrame(description_frame, background='green')
+ability_frame.place(relx=0,rely=0,relheight=0.25,relwidth=1,anchor='nw')
+moves_frame = tk.LabelFrame(description_frame,background='red')
+moves_frame.place(relx=0,rely=0.25,relheight=0.25,relwidth=1,anchor='nw')
+weakness_frame = tk.LabelFrame(description_frame,background='cyan')
+weakness_frame.place(relx=0,rely=0.5,relheight=0.25,relwidth=1,anchor='nw')
+pokedex_frame = tk.LabelFrame(description_frame,background='white')
+pokedex_frame.place(relx=0,rely=0.75,relheight=0.25,relwidth=1,anchor='nw')
 
 # Structure of the start frame
 logo = tk.PhotoImage(file="images/GUI/Logo_res.png")
