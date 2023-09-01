eed066ea0ea72f51d9b43d97040aa3452b81206a
MastroCC8 <nicolamastrorilli33@gmail.com>
2023-09-01 16:54:28 +0200

    Added labels to be filled

--- code/GUI/GUI.py
+++ code/GUI/GUI.py
@@ -9,7 +9,7 @@ def start():
 '''
     
 #Nicola's command that ChatGPT cancelled but it doesn't run yet so we don't know if it works
-def kyc(cen, cor, edg, sur, a_notebook, card_depiction_frame, card_frame, centering_frame, edges_frame, corners_frame, surface_frame, mean_frame):
+def kyc(cen, cor, edg, sur, a_notebook, card_depiction_frame, card_frame, centering_frame, edges_frame, corners_frame, surface_frame, mean_frame,inner_name_frame,set_frame,set_base_var,fossil_var,jungle_var,pv_frame,type_frame,ability_name_frame,ability_effect_frame,move1_name_frame,move1_cost_frame,move1_effect_frame,move1_damage_frame,move2_name_frame,move2_cost_frame,move2_effect_frame,move2_damage_frame):
         a_notebook.tab(card_depiction_frame, state="normal")
         a_notebook.select(card_depiction_frame)
         card = Image.open("code/card.jpg")
@@ -29,38 +29,67 @@ def kyc(cen, cor, edg, sur, a_notebook, card_depiction_frame, card_frame, center
         mean=(cen+cor+edg+sur)//4
         mean_label=tk.Label(mean_frame,text="{}".format(mean),font='80')  #to be filled by the output of the grading
         mean_label.place(relx=0.5,rely=0.5,anchor='center')
+        pokemon_name_label=tk.Label(inner_name_frame,text='',font='Helvetica 20') #to be filled by the pokemon name
+        pokemon_name_label.pack()
+        set_name_label=tk.Label(set_frame,text='',font='Helvetica 20')
+        set_name_label.pack()
+        if set_base_var.get()==1:
+            set_name_label.configure(text='Set Base')
+        elif fossil_var.get()==1:
+            set_name_label.configure(text='Fossil')
+        elif  jungle_var.get()==1:
+            set_name_label.configure(text='Jungle')
+        pv_number_label=tk.Label(pv_frame,text='',font='Helvetica 10') #to be filled by the number of PV
+        pv_number_label.place(relx=0.5,rely=0.5,anchor="center")
+        type_name_label=tk.Label(type_frame,text='',font='Helvetica 10') #to be filled by the pokemon type
+        type_name_label.place(relx=0.5,rely=0.5,anchor="center")
+        ability_name_label=tk.Label(ability_name_frame,text='',font='Helvetica 20') #to be filled with ability name
+        ability_name_label.place(relx=0.5,rely=0.5,anchor='center')
+        ability_effect_label=tk.Label(ability_effect_frame,text='',font='Helvetica 10') #to be filled with ability effect
+        ability_effect_label.place(relx=0.5,rely=0.5,anchor='center')
+        move1_name_label=tk.Label(move1_name_frame,text='',font='Helvetica 20') #to be filled with move1 name 
+        move1_name_label.place(relx=0.5,rely=0.5,anchor="center")
+        move1_cost_label=tk.Label(move1_cost_frame,text='',font='Helvetica 10') #to be filled with move1 cost
+        move1_cost_label.place(relx=0.5,rely=0.5,anchor="center")
+        move1_effect_label=tk.Label(move1_effect_frame,text='',font='Helvetica 10') #to be filled with move1 effect
+        move1_effect_label.place(relx=0.5,rely=0.5,anchor="center")
+        move1_damage_label=tk.Label(move1_damage_frame,text='',font='Helvetica 10') #to be filled with move1 damage
+        move1_damage_label.place(relx=0.5,rely=0.5,anchor="center")
+        move2_name_label=tk.Label(move2_name_frame,text='',font='Helvetica 20') #to be filled with move2 name
+        move2_name_label.place(relx=0.5,rely=0.5,anchor="center")
+        move2_cost_label=tk.Label(move2_cost_frame,text='',font='Helvetica 10') #to be filled with move2 cost
+        move2_cost_label.place(relx=0.5,rely=0.5,anchor="center")
+        move2_effect_label=tk.Label(move2_effect_frame,text='',font='Helvetica 10') #to be filled with move2 effect
+        move2_effect_label.place(relx=0.5,rely=0.5,anchor="center")
+        move2_damage_label=tk.Label(move2_damage_frame,text='',font='Helvetica 10') #to be filled with move2 damage
+        move2_damage_label.place(relx=0.5,rely=0.5,anchor="center")
         pass
-    
 
-def home(a_notebook, starting_frame, card_depiction_frame):
-    a_notebook.select(starting_frame)
-    a_notebook.tab(card_depiction_frame, state="hidden")
-
-def set_base_toggle(set_base_checkbox, fossil_checkbox, jungle_checkbox, var1, var2, var3, logo_button):
+def set_base_toggle(set_base_checkbox, fossil_checkbox, jungle_checkbox, set_base_var,fossil_var, jungle_var, logo_button):
      set_base_checkbox.toggle()
      fossil_checkbox.deselect()
      jungle_checkbox.deselect()
-     if var1.get()==1 or var2.get()==1 or var3.get()==1:
+     if set_base_var.get()==1 or fossil_var.get()==1 or jungle_var.get()==1:
         logo_button.configure(state="normal")
      else:
          logo_button.configure(state='disabled')
      
     
-def fossil_toggle(fossil_checkbox, jungle_checkbox, set_base_checkbox, var1, var2, var3, logo_button):
+def fossil_toggle(fossil_checkbox, jungle_checkbox, set_base_checkbox, set_base_var,fossil_var,jungle_var, logo_button):
     fossil_checkbox.toggle()
     jungle_checkbox.deselect()
     set_base_checkbox.deselect()
-    if var1.get()==1 or var2.get()==1 or var3.get()==1:
+    if set_base_var.get()==1 or fossil_var.get()==1 or jungle_var.get()==1:
         logo_button.configure(state="normal")
     else:
         logo_button.configure(state='disabled')
      
 
-def jungle_toggle(jungle_checkbox, fossil_checkbox, set_base_checkbox, var1, var2, var3, logo_button):
+def jungle_toggle(jungle_checkbox, fossil_checkbox, set_base_checkbox, set_base_var,fossil_var,jungle_var, logo_button):
     jungle_checkbox.toggle()
     fossil_checkbox.deselect()
     set_base_checkbox.deselect()
-    if var1.get()==1 or var2.get()==1 or var3.get()==1:
+    if set_base_var.get()==1 or fossil_var.get()==1 or jungle_var.get()==1:
         logo_button.configure(state="normal")
     else:
         logo_button.configure(state='disabled')
@@ -92,34 +121,34 @@ def GUI(cen, cor, edg, sur):
     logo = Image.open("images/GUI/Logo_res.png")
     resized_logo=logo.resize((400,400))
     new_logo=ImageTk.PhotoImage(resized_logo)
-    logo_button = tk.Button(starting_frame, image=new_logo, command=lambda :kyc(cen, cor, edg, sur, a_notebook, card_depiction_frame, card_frame, centering_frame, edges_frame, corners_frame, surface_frame, mean_frame), borderwidth=0,state="disabled")
+    logo_button = tk.Button(starting_frame, image=new_logo, command=lambda :kyc(cen, cor, edg, sur, a_notebook, card_depiction_frame, card_frame, centering_frame, edges_frame, corners_frame, surface_frame, mean_frame,inner_name_frame,set_frame,set_base_var,fossil_var,jungle_var,pv_frame,type_frame,ability_name_frame,ability_effect_frame,move1_name_frame,move1_cost_frame,move1_effect_frame,move1_damage_frame,move2_name_frame,move2_cost_frame,move2_effect_frame,move2_damage_frame), borderwidth=0,state="disabled")
     logo_button.place(relx=0.5, rely=0.4, anchor="center")
     #Set Base check structure
-    var1=tk.IntVar()
-    set_base_checkbox=tk.Checkbutton(starting_frame,variable=var1,border=0,state="disabled",relief="flat")
+    set_base_var=tk.IntVar()
+    set_base_checkbox=tk.Checkbutton(starting_frame,variable=set_base_var,border=0,state="disabled",relief="flat")
     set_base_checkbox.place(relx=0.4,rely=0.85,anchor="center")
     setbase_logo = Image.open('images\GUI\setbase.png')
     resized_setbase=setbase_logo.resize((25,25))
     new_setbase_logo=ImageTk.PhotoImage(resized_setbase)
-    set_base_button=tk.Button(starting_frame,image=new_setbase_logo,border=1,command=lambda: set_base_toggle(set_base_checkbox, fossil_checkbox, jungle_checkbox, var1, var2, var3, logo_button))
+    set_base_button=tk.Button(starting_frame,image=new_setbase_logo,border=1,command=lambda: set_base_toggle(set_base_checkbox, fossil_checkbox, jungle_checkbox, set_base_var,fossil_var,jungle_var, logo_button))
     set_base_button.place(relx=0.397,rely=0.8,anchor="center")
     #Fossil check structure
-    var2=tk.IntVar()
-    fossil_checkbox=tk.Checkbutton(starting_frame,variable=var2,border=0,state="disabled",relief='flat')
+    fossil_var=tk.IntVar()
+    fossil_checkbox=tk.Checkbutton(starting_frame,variable=fossil_var,border=0,state="disabled",relief='flat')
     fossil_checkbox.place(relx=0.5,rely=0.85,anchor="center")
     fossil_logo = Image.open('images\\GUI\\fossil.png')
     resized_fossil=fossil_logo.resize((25,25))
     new_fossil_logo=ImageTk.PhotoImage(resized_fossil)
-    fossil_button=tk.Button(starting_frame,image=new_fossil_logo,border=1,command=lambda: fossil_toggle(fossil_checkbox, jungle_checkbox, set_base_checkbox, var1, var2, var3, logo_button))
+    fossil_button=tk.Button(starting_frame,image=new_fossil_logo,border=1,command=lambda: fossil_toggle(fossil_checkbox, jungle_checkbox, set_base_checkbox, set_base_var,fossil_var,jungle_var, logo_button))
     fossil_button.place(relx=0.497,rely=0.8,anchor="center")
     #Jungle check structure
-    var3=tk.IntVar()
-    jungle_checkbox=tk.Checkbutton(starting_frame,variable=var3,border=0,state="disabled",relief="flat")
+    jungle_var=tk.IntVar()
+    jungle_checkbox=tk.Checkbutton(starting_frame,variable=jungle_var,border=0,state="disabled",relief="flat")
     jungle_checkbox.place(relx=0.6,rely=0.85,anchor="center")
     jungle_logo = Image.open('images\GUI\jungle.png')
     resized_jungle=jungle_logo.resize((25,25))
     new_jungle_logo=ImageTk.PhotoImage(resized_jungle)
-    jungle_button=tk.Button(starting_frame,image=new_jungle_logo,border=1,command=lambda: jungle_toggle(jungle_checkbox, fossil_checkbox, set_base_checkbox, var1, var2, var3, logo_button))
+    jungle_button=tk.Button(starting_frame,image=new_jungle_logo,border=1,command=lambda: jungle_toggle(jungle_checkbox, fossil_checkbox, set_base_checkbox, set_base_var,fossil_var,jungle_var, logo_button))
     jungle_button.place(relx=0.597,rely=0.8,anchor="center")
 
     # Descriptive label
@@ -161,12 +190,21 @@ def GUI(cen, cor, edg, sur):
      #Frames in name frame
     inner_name_frame=tk.Frame(name_frame)
     inner_name_frame.place(relx= 0,rely=0,relheight=1,relwidth=0.333)
+    inner_name_label=tk.Label(inner_name_frame,text='Pokemon Name',font='Helvetica 8')
+    inner_name_label.pack(pady=6)
     set_frame=tk.Frame(name_frame)
     set_frame.place(relx=0.333,rely=0,relheight=1,relwidth=0.333)
+    set_descriptive_label=tk.Label(set_frame,text='Set Name',font='Helvetica 8')
+    set_descriptive_label.pack(pady=6)
     pv_frame=tk.Frame(name_frame)
     pv_frame.place(relx=0.666,rely=0,relheight=0.5,relwidth=0.333)
+    pv_label=tk.Label(pv_frame,text='PV:',font='Helvetica 10')
+    pv_label.place(relx=0.25,rely=0.5,anchor="center")
     type_frame=tk.Frame(name_frame)
     type_frame.place(relx=0.666,rely=0.5,relheight=0.5,relwidth=0.333)
+    type_label=tk.Label(type_frame,text='Type:',font='Helvetica 10')
+    type_label.place(relx=0.25,rely=0.5,anchor="center")
+
     #Description frame:
     description_frame = tk.LabelFrame(card_depiction_frame, border=1)
     description_frame.place(relx=0.35, rely=0.3, relheight=0.65,relwidth=0.65, anchor="nw")
